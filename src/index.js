2b013897e8786106fb5d27996ae0cd1ff6ff4150
project-sa-git <50135286+project-sa-git@users.noreply.github.com>
2022-09-27 20:50:38 +0900

    完了ボタン・削除ボタンの追加

--- src/index.js
+++ src/index.js
@@ -7,14 +7,24 @@ const onClickAdd = () => {
 
   // div生成("div"タグを生成する)
   const div = document.createElement("div");
-  div.className = "list-row" // コンソールで<div class="list-row"></div>
+  div.className = "list-row"; // コンソールで<div class="list-row"></div>
 
   // liタグ生成
   const li = document.createElement("li");
   li.innerText = inputText; // コンソールで<li>dddd</li>
 
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
   // divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // 未完了リストに追加（画面の要素を書き換える）
   document.getElementById("incomplete-list").appendChild(div);
